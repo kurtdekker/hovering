308c1bf6cc0beeece965939659d8a19a54d0d690
Kurtina <kurtdekker@gmail.com>
2018-10-06 09:42:48 -0700

    SimpleHover: let's make a nice simple Z+Rotation drive for the multi-hover ship
    - also adds more material, more geometry, and two additional lifters on the edges (midpoint)
    - add more objects to run over as well
    - add in a simple chase camera too

---
+++ simplehover/Assets/SimpleHover/SimpleCamera.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleCamera : MonoBehaviour
+{
+	public	Transform	target;
+
+	public	float		Snappiness;
+
+	public	bool		StayBehind;
+
+	void Reset()
+	{
+		Snappiness = 2.0f;
+	}
+
+	Vector3 offset;
+
+	Vector3 DesiredPosition;
+
+	void Start ()
+	{
+		offset = transform.position - target.position;
+	}
+	
+	void Update ()
+	{
+		Vector3 UsableOffset = offset;
+
+		if (StayBehind)
+		{
+			float YRotation = target.rotation.eulerAngles.y;
+
+			UsableOffset = Quaternion.Euler( 0, YRotation, 0) * UsableOffset;
+		}
+
+		DesiredPosition = target.position + UsableOffset;
+
+		transform.position = Vector3.Lerp(
+			transform.position, DesiredPosition, Snappiness * Time.deltaTime);
+
+		transform.LookAt( target);
+	}
+}
