cc8db1e5f24d496a30b24bc4d634b4c1f9b5dd1c
Kurtina <kurtdekker@gmail.com>
2018-10-13 10:21:16 -0700

    TeleportToNamedGameObject script

---
+++ hovering/Assets/Scripts/TeleportToNamedGameObject.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeleportToNamedGameObject : MonoBehaviour
+{
+	public string GameObjectNameToSearchFor = "<none>";		
+
+	void Update ()
+	{
+		GameObject go = GameObject.Find(GameObjectNameToSearchFor);
+
+		if (go)
+		{
+			transform.position = go.transform.position;
+			transform.rotation = go.transform.rotation;
+			
+			Destroy(this);
+		}
+	}
+}
